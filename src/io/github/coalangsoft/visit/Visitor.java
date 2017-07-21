ae5cd1e9dc2c8da437e09a1a8a2ed9fb7e3d808c
NetLamp <oplpublishergerman@gmail.com>
2017-07-21 15:24:15 +0200

    Refactor

--- src/io/github/coalangsoft/visit/Visitor.java
+++ src/io/github/coalangsoft/visit/Visitor.java
@@ -1,7 +1,7 @@
 package io.github.coalangsoft.visit;
 
 import io.github.coalangsoft.lib.data.Func;
-import io.github.coalangsoft.lib.data.ImutablePair;
+import io.github.coalangsoft.lib.data.ImmutablePair;
 import io.github.coalangsoft.lib.data.Pair;
 
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ public class Visitor {
 	private ArrayList<Pair<Class<?>, Func<Object,Void>>> functions;
 	
 	public void addFunction(Class<?> c, Func<Object,Void> f){
-		functions.add(new ImutablePair<Class<?>, Func<Object,Void>>(c, f));
+		functions.add(new ImmutablePair<Class<?>, Func<Object,Void>>(c, f));
 	}
 
 	public void handle(Object o) {
